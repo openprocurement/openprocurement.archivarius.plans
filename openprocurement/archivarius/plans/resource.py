746ed41305876b3148cdf01f906321fe0b4dc87c
Roman Kozlovskyi <kroman0@quintagroup.com>
2017-07-03 15:32:48 +0300

    Update sandbox filter rules

--- openprocurement/archivarius/plans/resource.py
+++ openprocurement/archivarius/plans/resource.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
 from iso8601 import parse_date
 from datetime import timedelta
 
-TIMEDELTA = timedelta(days=455)
+SANDBOX_MODE = os.environ.get('SANDBOX_MODE', False)
+TIMEDELTA = timedelta(days=20) if SANDBOX_MODE else timedelta(days=455)
 
 
 def plan_filter(item, time):
