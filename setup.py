a08a1fbce39552566b0c5c4033d73e2f937c4d9b
Roman Kozlovskyi <kroman0@quintagroup.com>
2017-07-27 13:19:20 +0300

    Prepare openprocurement.archivarius.plans 1.0.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ entry_points = {
 }
 
 setup(name='openprocurement.archivarius.plans',
-      version='1.0.0dev',
+      version='1.0',
       description='openprocurement.archivarius.plans',
       long_description=README,
       classifiers=[
